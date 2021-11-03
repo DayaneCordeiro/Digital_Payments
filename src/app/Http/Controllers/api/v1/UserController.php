55155176296d5f804871a8ac7bf1f9a156c2699b
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-11-02 23:39:07 -0300

    Digital Payments :: Refactoring User Controller File

--- src/app/Http/Controllers/api/v1/UserController.php
+++ src/app/Http/Controllers/api/v1/UserController.php
@@ -2,30 +2,17 @@
 
 namespace App\Http\Controllers\api\v1;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-// use GuzzleHttp\Exception\GuzzleException;
-// use GuzzleHttp\Client;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Request;
 
+use App\Http\Controllers\api\v1\WalletController;
 use App\Models\User;
+use App\Models\Wallet;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index() {
-        // Forma de usar os mocks
-        $response = Http::get('https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6');
-        return $response;
-        // $users = User::all();
-        // return response()->json($users, 200);
-    }
-
     /**
      * Store a newly created user in storage.
      *
@@ -34,71 +21,168 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        // Validating type
-        $acceptableTypes = ['common', 'shopkeeper'];
+        try {
+            // Validating type
+            $acceptableTypes = ["common", "shopkeeper"];
 
-        if (!in_array($request->type, $acceptableTypes)) {
-            return response()->json(["message" => "User type must be common or shopkeeper."], 400);
+            if (isset($request->type) && !in_array($request->type, $acceptableTypes)) {
+                $error = ["message" => "User type must be common or shopkeeper."];
+                return response()->json($error, 400);
+            }
+
+            // Validating status
+            $acceptableStatus = ["active", "inactive"];
+
+            if (isset($request->status) && !in_array($request->status, $acceptableStatus)) {
+                $error = ["message" => "User status must be active or inactive."];
+                return response()->json($error, 400);
+            }
+
+            // Validating CPF and CNPJ
+            $userWithSameCpf = User::where("cpf_cnpj", $request->cpf_cnpj)->first();
+            
+            if ($userWithSameCpf) {
+                $error = ["message" => "CPF or CNPJ is already in use."];
+                return response()->json($error, 400);
+            }
+
+            $documentValidation = ($request->type == "common") ? $this->validateCpf($request->cpf_cnpj) : $this->validateCnpj($request->cpf_cnpj);
+
+            if (!$documentValidation) {
+                $error = ["message" => "CPF or CNPJ is invalid."];
+                return response()->json($error, 400);
+            }
+
+            // Validating email
+            $userWithSameEmail = User::where("email", $request->email)->first();
+
+            if ($userWithSameEmail) {
+                $error = ["message" => "Email is already in use."];
+                return response()->json($error, 400);
+            }
+
+            // Native Validation rulers
+            $rules = [
+                "name"     => "required",
+                "email"    => "required|email",
+                "password" => "required",
+                "cpf_cnpj" => "required|numeric",
+                "type"     => "required",
+                "status"   => "required",
+            ];
+
+            // Validation messages
+            $messages = [
+                "name.required"     => "Name is required.",
+                "email.required"    => "Email is required.",
+                "email.email"       => "Invalid email.",
+                "password.required" => "Password is required.",
+                "cpf_cnpj.required" => "CPF or CNPJ is required.",
+                "cpf_cnpj.numeric"  => "CPF or CNPJ must be numeric.",
+                "type.required"     => "Type is required.",
+                "type.required"     => "Status is required."
+            ];
+            
+            // Taking only the necessary data
+            $requestData = $request->only(["name", "email", "password", "cpf_cnpj", "type", "status"]);
+            
+            // Hashing password
+            $requestData["password"] = md5($request["password"]);
+            
+            // First parameter:  Data to be validated
+            // Second parameter: Rules that will be applied
+            // Third parameter:  Matching messages
+            $validator = Validator::make($requestData, $rules, $messages);
+            
+            if ($validator->fails()) return response()->json($validator->errors(), 400);
+            
+            $user = User::create($requestData);
+
+            // Creating user wallet
+            if ($user) {
+                $wallet = Http::post('http://nginx/api/v1/wallets', [
+                    "user_id" => $user->id,
+                    "balance" => 0,
+                    "status"  => "active"
+                ]);
+                
+                return response()->json($user, 201);
+            }
+        } catch (Exception $e) {
+            return response()->json(["Message" => $e->getMessage()], 502);
         }
+    }
 
-        // Validating CPF and CNPJ
-        $userWithSameCpf = User::where('cpf_cnpj', $request->cpf_cnpj)->first();
-        
-        if ($userWithSameCpf) {
-            return response()->json(["message" => "CPF or CNPJ is already in use."], 400);
+    /**
+     * Inactive the specified resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function inactive(Request $request)
+    {
+        try {
+            $user = User::find($request->id);
+
+            if (is_null($user)) {
+                $error = ["message" => "User not found."];
+                return response()->json($error, 400);
+            }
+
+            if ($user->status !== "active") {
+                $error = ["message" => "User is already inactive."];
+                return response()->json($error, 400);
+            }
+
+            $user->update(["status" => "inactive"]);
+
+            // Inactive user's wallet
+            $wallet = Wallet::where("user_id", $request->id)->first();
+            
+            Http::put('http://nginx/api/v1/wallets_inactive', [
+                "id" => $wallet->id
+            ]);
+
+            return response()->json(null, 204);
+        } catch (Exception $e) {
+            return response()->json(["Message" => $e->getMessage()], 502);
         }
+    }
 
-        $documentValidation = ($request->type == "common") ? $this->validateCpf($request->cpf_cnpj) : $this->validateCnpj($request->cpf_cnpj);
-
-        if (!$documentValidation) {
-            return response()->json(["message" => "CPF or CNPJ is invalid."], 400);
+    /**
+     * Active the specified resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function active(Request $request)
+    {
+        try {
+            $user = User::find($request->id);
+    
+            if (is_null($user)) {
+                $error = ["message" => "User not found."];
+                return response()->json($error, 400);
+            }
+    
+            if ($user->status !== "inactive") {
+                $error = ["message" => "User is already active."];
+                return response()->json($error, 400);
+            }
+    
+            $user->update(["status" => "active"]);
+    
+            // Active user's wallet
+            $wallet = Wallet::where("user_id", $request->id)->first();
+            
+            Http::put('http://nginx/api/v1/wallets_active', [
+                "id" => $wallet->id
+            ]);
+    
+            return response()->json(null, 204);
+        } catch (Exception $e) {
+            return response()->json(["Message" => $e->getMessage()], 502);
         }
-
-        // Validating email
-        $userWithSameEmail = User::where('email', $request->email)->first();
-
-        if ($userWithSameEmail) {
-            return response()->json(["message" => "Email is already in use."],400);
-        }
-
-        // Validation rulers
-        $rules = [
-            'name'     => 'required',
-            'email'    => 'required|email',
-            'password' => 'required',
-            'cpf_cnpj' => 'required|numeric',
-            'type'     => 'required',
-        ];
-
-        // Validation messages
-        $messages = [
-            'name.required'     => 'Name is required.',
-            'email.required'    => 'Email is required.',
-            'email.email'       => 'Invalid email.',
-            'password.required' => 'Password is required.',
-            'cpf_cnpj.required' => 'CPF or CNPJ is required.',
-            'cpf_cnpj.numeric'  => 'CPF or CNPJ must be numeric.',
-            'type.required'     => 'Type is required.'
-        ];
-        
-        // Taking only the necessary data
-        $requestData = $request->only(['name', 'email', 'password', 'cpf_cnpj', 'type']);
-        
-        // Hashing password
-        $requestData['password'] = password_hash($request['password'], PASSWORD_DEFAULT);
-        
-        // First parameter:  Data to be validated
-        // Second parameter: Rules that will be applied
-        // Third parameter:  Matching messages
-        $validator = Validator::make($requestData, $rules, $messages);
-        
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
-        
-        $user = User::create($requestData);
-
-        return response()->json($user, 201);
     }
 
     /**
@@ -109,17 +193,18 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        $user = User::find($id);
-
-        if (is_null($user)) {
-            $error = [
-                'message' => 'User not found.'
-            ];
-
-            return response()->json($error, 400);
+        try {
+            $user = User::find($id);
+    
+            if (is_null($user)) {
+                $error = ["message" => "User not found."];
+                return response()->json($error, 400);
+            }
+    
+            return response()->json($user, 200);
+        } catch (Exception $e) {
+            return response()->json(["Message" => $e->getMessage()], 502);
         }
-
-        return response()->json($user, 200);
     }
 
     /**
@@ -131,66 +216,72 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $user = User::find($id);
+        try {
+            // Validating user
+            $user = User::find($id);
+    
+            if (is_null($user)) {
+                $error = ["message" => "User not found."];
+                return response()->json($error, 400);
+            }
 
-        if (is_null($user)) {
-            $error = [
-                'message' => 'User not found.'
+            // Validating type
+            $acceptableTypes = ["common", "shopkeeper"];
+
+            if (isset($request->type) && !in_array($request->type, $acceptableTypes)) {
+                $error = ["message" => "User type must be common or shopkeeper."];
+                return response()->json($error, 400);
+            }
+
+            // Validating status
+            $acceptableStatus = ["active", "inactive"];
+
+            if (isset($request->status) && !in_array($request->status, $acceptableStatus)) {
+                $error = ["message" => "User status must be active or inactive."];
+                return response()->json($error, 400);
+            }
+
+            // Validating CPF and CNPJ
+            $documentValidation = ($request->type == "common") ? $this->validateCpf($request->cpf_cnpj) : $this->validateCnpj($request->cpf_cnpj);
+
+            if (!$documentValidation) {
+                $error = ["message" => "CPF or CNPJ is invalid."];
+                return response()->json($error, 400);
+            }
+    
+            $rules = [
+                "name"     => "required",
+                "email"    => "required|email",
+                "password" => "required",
+                "cpf_cnpj" => "required",
+                "type"     => "required",
             ];
-
-            return response()->json($error, 400);
-        }
-
-        $rules = [
-            'name'     => 'required',
-            'email'    => 'required|email',
-            'password' => 'required',
-            'cpf_cnpj' => 'required',
-            'type'     => 'required',
-        ];
-
-        $messages = [
-            'name.required'     => 'Name is required.',
-            'email.required'    => 'Email is required.',
-            'email.email'       => 'Invalid email.',
-            'password.required' => 'Password is required.',
-            'cpf_cnpj.required' => 'CPF or CNPJ is required.',
-            'type.required'     => 'Type is required.'
-        ];
-
-        $requestData = $request->only(['name', 'email', 'password', 'cpf_cnpj', 'type']);
-
-        $validator = Validator::make($requestData, $rules, $messages);
-
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
-
-        $user->update($requestData);
-
-        return response()->json($user, 201);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $user = User::find($id);
-
-        if(is_null($user)) {
-            $error = [
-                'message' => 'User not found.'
+    
+            $messages = [
+                "name.required"     => "Name is required.",
+                "email.required"    => "Email is required.",
+                "email.email"       => "Invalid email.",
+                "password.required" => "Password is required.",
+                "cpf_cnpj.required" => "CPF or CNPJ is required.",
+                "type.required"     => "Type is required."
             ];
-
-            return response()->json($error, 400);
+    
+            $requestData = $request->only(["name", "email", "password", "cpf_cnpj", "type"]);
+    
+            // Hashing password
+            $requestData["password"] = md5($request["password"]);
+    
+            $validator = Validator::make($requestData, $rules, $messages);
+    
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 400);
+            }
+    
+            $user->update($requestData);
+    
+            return response()->json($user, 201);
+        } catch (Exception $e) {
+            return response()->json(["Message" => $e->getMessage()], 502);
         }
-
-        $user->delete();
-
-        return response()->json(null, 204);
     }
 }
