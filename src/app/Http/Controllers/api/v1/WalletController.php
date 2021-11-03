12f36a20b282519dcbe23f91e80a2867ff575348
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-11-03 01:05:46 -0300

    Digital Payments :: Refactoring Wallets Controller

--- src/app/Http/Controllers/api/v1/WalletController.php
+++ src/app/Http/Controllers/api/v1/WalletController.php
@@ -2,26 +2,15 @@
 
 namespace App\Http\Controllers\api\v1;
 
+use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 
 use App\Models\Wallet;
 use App\Models\User;
 
 class WalletController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $wallets = Wallet::all();
-        return response()->json($wallets, 200);
-    }
-
     /**
      * Store a newly created resource in storage.
      *
@@ -30,35 +19,122 @@ class WalletController extends Controller
      */
     public function store(Request $request)
     {
-        // Validation rulers -> talvez fazer tudo manualmente vide documentação salva nos favoritos
-        $rules = [
-            'user_id' => 'required',
-            'balance' => 'required',
-            'status'  => 'required'
-        ];
+        try {
+            $user = User::find($request->user_id);
 
-        // Validation messages
-        $messages = [
-            'user_id.required' => 'user_id is required.',
-            'balance.required' => 'balance is required.',
-            'status.required'  => 'status is required.'
-        ];
+            // Checks if user exists
+            if (!$user) {
+                $error = ["message" => "User not found."];
+                return response()->json($error, 400);
+            }
 
-        // Taking only the necessary data
-        $requestData = $request->only(['user_id', 'balance', 'status']);
+            // Validation rulers -> talvez fazer tudo manualmente vide documentação salva nos favoritos
+            $rules = [
+                'user_id' => 'required',
+                'balance' => 'required|numeric',
+                'status'  => 'required'
+            ];
 
-        // First parameter: Data to be validated
-        // Second parameter: rules that will be applied
-        // Third parameter: matching messages
-        $validator = Validator::make($requestData, $rules, $messages);
+            // Validation messages
+            $messages = [
+                'user_id.required' => 'User id is required.',
+                'balance.required' => 'Balance is required.',
+                'balance.numeric'  => 'Balance must be numeric.',
+                'status.required'  => 'Status is required.'
+            ];
 
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
+            // Taking only the necessary data
+            $requestData = $request->only(['user_id', 'balance', 'status']);
+
+            // First parameter: Data to be validated
+            // Second parameter: rules that will be applied
+            // Third parameter: matching messages
+            $validator = Validator::make($requestData, $rules, $messages);
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 400);
+            }
+
+            $wallet = Wallet::create($requestData);
+
+            return response()->json($wallet, 201);
+        } catch(Exception $e) {
+            return response()->json(["Message" => $e->getMessage()], 502);
+        }        
+    }
+
+    /**
+     * Inactive the specified resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function inactive(Request $request) {
+        $wallet = Wallet::find($request->id);
+
+        if (is_null($wallet)) {
+            $error = ["message" => "Wallet not found."];
+            return response()->json($error, 400);
         }
 
-        $wallet = Wallet::create($requestData);
+        if ($wallet->status !== "active") {
+            $error = ["message" => "Wallet is already inactive."];
+            return response()->json($error, 400);
+        }
 
-        return response()->json($wallet, 201);
+        $wallet->update(["status" => "inactive"]);
+
+        return response()->json(null, 204);
+    }
+
+    /**
+     * Active the specified resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function active(Request $request) {
+        $wallet = Wallet::find($request->id);
+
+        if (is_null($wallet)) {
+            $error = ["message" => "Wallet not found."];
+            return response()->json($error, 400);
+        }
+
+        if ($wallet->status !== "inactive") {
+            $error = ["message" => "Wallet is already active."];
+            return response()->json($error, 400);
+        }
+
+        $wallet->update(["status" => "active"]);
+
+        return response()->json(null, 204);
+    }
+
+    /**
+     * Insert money to the specified resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function deposite(Request $request)
+    {
+        try {
+            $wallet = Wallet::find($request->id);
+
+            // Checks if wallet exists
+            if (!$wallet) {
+                $error = ["message" => "Wallet not found."];
+                return response()->json($error, 400);
+            }
+
+            $additionValue = $request->value + $wallet->balance;
+            $wallet->update(["balance" => $additionValue]);
+
+            return response()->json($wallet, 201);
+        } catch(Exception $e) {
+            return response()->json(["Message" => $e->getMessage()], 502);
+        }     
     }
 
     /**
@@ -69,89 +145,17 @@ class WalletController extends Controller
      */
     public function show($id)
     {
-        $wallet = Wallet::find($id);
-
-        if (is_null($wallet)) {
-            $error = [
-                'message' => 'wallet not found.'
-            ];
-
-            return response()->json($error, 400);
-        }
-
-        return response()->json($wallet, 200);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $wallet = Wallet::find($id);
-
-        if (is_null($wallet)) {
-            $error = [
-                'message' => 'Wallet not found.'
-            ];
-
-            return response()->json($error, 400);
-        }
-
-        // Validation rulers -> talvez fazer tudo manualmente vide documentação salva nos favoritos
-        $rules = [
-            'user_id' => 'required',
-            'balance' => 'required',
-            'status'  => 'required'
-        ];
-
-        // Validation messages
-        $messages = [
-            'user_id.required' => 'user_id is required.',
-            'balance.required' => 'balance is required.',
-            'status.required'  => 'status is required.'
-        ];
-
-        // Taking only the necessary data
-        $requestData = $request->only(['user_id', 'balance', 'status']);
-
-        // First parameter: Data to be validated
-        // Second parameter: rules that will be applied
-        // Third parameter: matching messages
-        $validator = Validator::make($requestData, $rules, $messages);
-
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
-
-        $wallet->update($requestData);
-
-        return response()->json($wallet, 201);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $wallet = Wallet::find($id);
-
-        if(is_null($wallet)) {
-            $error = [
-                'message' => 'Wallet not found.'
-            ];
-
-            return response()->json($error, 400);
-        }
-
-        $wallet->delete();
-
-        return response()->json(null, 204);
+        try {
+            $wallet = Wallet::find($id);
+    
+            if (is_null($wallet)) {
+                $error = ['message' => 'wallet not found.'];
+                return response()->json($error, 400);
+            }
+    
+            return response()->json($wallet, 200);
+        } catch(Exception $e) {
+            return response()->json(["Message" => $e->getMessage()], 502);
+        }  
     }
 }
